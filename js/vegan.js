e8450d4a91f384a3fd09b13021c1b73bd752c0ec
PhilippSchramm91 <philippschramm@posteo.de>
2015-04-15 04:55:04 +0200

    Update vegan.js

--- js/vegan.js
+++ js/vegan.js
@@ -2,7 +2,6 @@ $(window).resize(function() {
 	myFunction();
 });
 
-<<<<<<< HEAD
 function isMobile(){
 	// User-Agent-String auslesen
 	var UserAgent = navigator.userAgent.toLowerCase();
@@ -23,12 +22,6 @@ function isMobile(){
 
 function vegan(p){
 	
-=======
-function vegan(p){
-	
-	//alert(p);
-	
->>>>>>> gh-pages
 	var container=document.getElementsByClassName('vegan');
 	var anzahl=container.length;
 	var aktiv;
@@ -39,18 +32,8 @@ function vegan(p){
 			aktiv=i;
 		}
 	}
-<<<<<<< HEAD
 
 		
-=======
-	
-	//alert(aktiv);
-	//alert(container.length);
-	//alert(container[0].style.display);
-	
-	
-	
->>>>>>> gh-pages
 	if(p==true){
 		//Wechsel nach rechts
 		if(aktiv<container.length-1){
@@ -72,27 +55,18 @@ function vegan(p){
 	container[neu].style.display="block";
 
 	//Um Fenster anzupassen falls neuer Teil länger oder kürzer ist
-<<<<<<< HEAD
 	if(isMobile()==false){
 		var s = skrollr.init();
 		s.refresh($("#slide-vegan div"));
 	}
 	
-=======
-	var s = skrollr.init();
-	s.refresh($("#slide-vegan div"));
->>>>>>> gh-pages
 	myFunction();
 	window.location.href="#slide-vegan";
 }
 
 function impressionen(p){
 	
-<<<<<<< HEAD
 	//alert("impressionen"+p);
-=======
-	//alert(p);
->>>>>>> gh-pages
 	
 	var container=document.getElementsByClassName('impressionen');
 	var anzahl=container.length;
@@ -126,7 +100,6 @@ function impressionen(p){
 	container[neu].style.display="block";
 
 	//Um Fenster anzupassen falls neuer Teil länger oder kürzer ist
-<<<<<<< HEAD
 	if(isMobile()==false){
 		var s = skrollr.init();
 		s.refresh($("#slide-impressionen div"));
@@ -135,13 +108,6 @@ function impressionen(p){
 	window.location.href="#slide-impressionen";
 }
 
-=======
-	var s = skrollr.init();
-	s.refresh($("#slide-impressionen div"));
-	myFunction();
-	window.location.href="#slide-impressionen";
-}
->>>>>>> gh-pages
 function navigation(p){
 	if($window.width()<=1000){
 		if(p==true){
@@ -151,10 +117,7 @@ function navigation(p){
 		}else{
 			//Navigation ist unsichtbar
 			$('.entry-header .inner').css('display','none');
-<<<<<<< HEAD
 
-=======
->>>>>>> gh-pages
 			
 		}
 	}
@@ -171,20 +134,6 @@ function navigationbtn(){
 	}
 }
 
-<<<<<<< HEAD
-=======
-function navigationbtneffect(p){
-	
-	if(p==true){
-		//Navigationbtn Effekt ist sichtbar
-		$('#nav-btn').css('box-shadow','0px 0px 5px grey');
-		
-	}else{
-		//Navigationbtn Effekt ist unsichtbar
-		$('#nav-btn').css('box-shadow','0px 0px 0px grey');
-	}
-}
->>>>>>> gh-pages
 
 function myFunction(){
 	
@@ -206,10 +155,7 @@ function myFunction(){
 	if(winW>1000){
 		//Normales Menü
 		$('#nav-btn').css('display','none');
-<<<<<<< HEAD
 		$('#fb-btn').css('display','none');
-=======
->>>>>>> gh-pages
 		
 		$('.entry-header .inner').css('display','block');
 		//$('.entry-header .inner').css('text-align','right');	
@@ -222,13 +168,8 @@ function myFunction(){
 		$('.entry-header .inner').css('top','20px');
 		
 		$('#nav-list').css('text-align','center');
-<<<<<<< HEAD
 		$('#nav-list').css('margin','0 0 0 55px');
 		$('#nav-list').css('padding','0.6em');
-=======
-		$('#nav-list').css('margin','0 0 0 50px');
-		$('#nav-list').css('padding','0.8em');
->>>>>>> gh-pages
 		$('#nav-list').css('background-color','transparent');
 		$('#nav-list').css('box-shadow','none');
 		//$('#nav-list').css('top','0');
@@ -246,20 +187,13 @@ function myFunction(){
 	}else{
 		//Mobiles Menü
 		$('#nav-btn').css('display','block');
-<<<<<<< HEAD
 		$('#fb-btn').css('display','block');
-=======
->>>>>>> gh-pages
 		
 		$('.entry-header .inner').css('display','none');
 		$('.entry-header .inner').css('text-align','right');	
 		$('.entry-header .inner').css('margin','0 0 0 0');
 		$('.entry-header .inner').css('width','0px');
-<<<<<<< HEAD
 		$('.entry-header .inner').css('min-width','200px');
-=======
-		$('.entry-header .inner').css('min-width','150px');
->>>>>>> gh-pages
 		$('.entry-header .inner').css('height','200px');
 		$('.entry-header .inner').css('position','fixed');
 		$('.entry-header .inner').css('right','0');
@@ -270,11 +204,7 @@ function myFunction(){
 		$('#nav-list').css('padding','0.2px');
 		$('#nav-list').css('background-color','#fff');
 		$('#nav-list').css('box-shadow','0px 0px 5px grey');
-<<<<<<< HEAD
 		$('#nav-list').css('top','0px');
-=======
-		$('#nav-list').css('top','10px');
->>>>>>> gh-pages
 		
 		$('#nav-list li').css('display','block');
 		$('#nav-list li').css('margin','15px auto 10px 10px');
@@ -285,37 +215,14 @@ function myFunction(){
 
 (function(n){n.viewportSize={},n.viewportSize.getHeight=function(){return t("Height")},n.viewportSize.getWidth=function(){return t("Width")};var t=function(t){var f,o=t.toLowerCase(),e=n.document,i=e.documentElement,r,u;return n["inner"+t]===undefined?f=i["client"+t]:n["inner"+t]!=i["client"+t]?(r=e.createElement("body"),r.id="vpw-test-b",r.style.cssText="overflow:scroll",u=e.createElement("div"),u.id="vpw-test-d",u.style.cssText="position:absolute;top:-1000px",u.innerHTML="<style>@media("+o+":"+i["client"+t]+"px){body#vpw-test-b div#vpw-test-d{"+o+":7px!important}}<\/style>",r.appendChild(u),i.insertBefore(r,e.head),f=u["offset"+t]==7?i["client"+t]:n["inner"+t],i.removeChild(r)):f=n["inner"+t],f}})(this);
 
-<<<<<<< HEAD
-=======
-/**
- * How to create a parallax scrolling website
- * Author: Petr Tichy
- * URL: www.ihatetomatoes.net
- * Article URL: http://ihatetomatoes.net/how-to-create-a-parallax-scrolling-website/
- */
->>>>>>> gh-pages
 
 ( function( $ ) {
 	// Setup variables
 	$window = $(window);
 	$body = $('body');
-<<<<<<< HEAD
 		
 	myFunction();
 	
-=======
-	
-	
-	myFunction();
-	
-	// Get window size
-	/*winH = $window.height();
-	
-	//Define the sizes of the slider
-	$('.bcg').css('min-height', winH);
-	$('.absoluteSlide').css('height', winH);
-	$('.absoluteSlideTall').css('height', winH*2);*/
->>>>>>> gh-pages
 	
     //FadeIn all sections   
 	$body.imagesLoaded( function() {
@@ -329,7 +236,6 @@ function myFunction(){
 			  
 		}, 800);
 	});
-<<<<<<< HEAD
 
 	//Mobile Anpassungen
 	console.log(isMobile());
@@ -383,19 +289,4 @@ function myFunction(){
 		
 	}
 	
-=======
-	
-	function adjustWindow(){
-		// Init Skrollr
-		var s = skrollr.init({
-		    render: function(data) {
-		    
-		        //Debugging - Log the current scroll position.
-		        //console.log(data.curTop);
-		    }
-		});
-	    
-	}
-		
->>>>>>> gh-pages
 } )( jQuery );
